63ec8d0639011515f4289a667ff38030fc714c9b
goldifsh <A01571719@tec.mx>
2026-04-29 11:56:38 -0600

    cypress tests

--- MtdrSpring/backend/src/main/frontend/cypress/e2e/login.cy.js
+++ MtdrSpring/backend/src/main/frontend/cypress/e2e/login.cy.js
@@ -4,13 +4,13 @@ describe('Inicio de Sesión', () => {
     cy.visit('/')
   })
 
-  it('Login válido redirige a /dashboard', () => {
+  it('Login válido redirige a /tasks', () => {
     cy.get('input[type="email"]').type('test2@ejemplo.com')
     cy.get('input[type="password"]').type('123456')
 
     cy.get('form').submit()
 
-    cy.url().should('include', '/dashboard')
+    cy.url().should('include', '/tasks')
   })
 
 })
