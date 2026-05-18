de2e38589b89eef24ead0abbe7a344d50ac95b34
goldifsh <A01571719@tec.mx>
2026-05-18 12:42:39 -0600

    Cypress

--- MtdrSpring/backend/src/main/frontend/cypress/e2e/login.cy.js
+++ MtdrSpring/backend/src/main/frontend/cypress/e2e/login.cy.js
@@ -4,13 +4,13 @@ describe('Inicio de Sesión', () => {
     cy.visit('/')
   })
 
-  it('Login válido redirige a /tasks', () => {
-    cy.get('input[type="email"]').type('test2@ejemplo.com')
-    cy.get('input[type="password"]').type('123456')
+  it('Login válido redirige a /home', () => {
+    cy.get('input[type="email"]').type('pruebas@pruebas.com')
+    cy.get('input[type="password"]').type('pruebas123')
 
     cy.get('form').submit()
 
-    cy.url().should('include', '/tasks')
+    cy.url().should('include', '/home')
   })
 
 })
